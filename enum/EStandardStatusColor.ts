379db08134e2d270c22ca5e040fdea7a62fd99e1
吴成 <1965417564@qq.com>
2023-11-29 10:15:09 +0800

    新增(enum):标准状态色枚举

---
+++ enum/EStandardStatusColor.ts
@@ -0,0 +1,34 @@
+/**
+ * 标准状态色枚举
+ */
+export enum EStandardStatusColor {
+  /**
+   * 警告色
+   * @description 对应#faad14
+   */
+  Warning = '#faad14',
+  
+  /**
+   * 错误色
+   * @description 对应#f5222d
+   */
+  Error = '#f5222d',
+
+  /**
+   * 成功色
+   * @description 对应#52c41a
+   */
+  Success = '#52c41a',
+
+  /**
+   * 失败色
+   * @description 对应#f5222d
+   */
+  Failed = '#f5222d',
+
+  /**
+   * 默认色
+   * @description 对应#1890ff
+   */
+  Default = '#1890ff',
+}
