cbbf0961db7bf4ea7890e61ab9faca2ce1fd6ca5
吴成 <1965417564@qq.com>
2023-11-29 10:17:46 +0800

    新增(enum):新增字段类型枚举

---
+++ enum/EFieldType.ts
@@ -0,0 +1,43 @@
+export enum EFieldType {
+  /**
+   * 字符串
+   * @description 对应String类型
+   */
+  String = 'String',
+
+  /**
+   * 数字
+   * @description 对应Number类型
+   */
+  Number = 'Number',
+
+  /**
+   * 布尔值
+   * @description 对应Boolean类型
+   */
+  Boolean = 'Boolean',
+
+  /**
+   * 日期
+   * @description 对应Date类型
+   */
+  Date = 'Date',
+
+  /**
+   * 对象
+   * @description 对应Object类型
+   */
+  Object = 'Object',
+
+  /**
+   * 数组
+   * @description 对应Array类型
+   */
+  Array = 'Array',
+
+  /**
+   * 枚举
+   * @description 对应Enum类型
+   */
+  Enum = 'Enum',
+}
