6f1b1c73cefb5ee15e7130b3d4fab89f332c6649
吴成 <1965417564@qq.com>
2023-12-05 16:33:05 +0800

    调整(interface):不再提供字段类型配置（暂定）

--- interface/IFieldConfig.ts
+++ interface/IFieldConfig.ts
@@ -2,12 +2,7 @@ import { EFieldType } from "../enum/EFieldType";
 
 export interface IFieldConfig {
   /**
-   * 字段描述名称
+   * # 字段描述名称
    */
   name: string;
-
-  /**
-   * 字段类型
-   */
-  type: EFieldType;
 }
