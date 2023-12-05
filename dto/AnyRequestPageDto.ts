860e05981dbf52569f17bdc25128c59e96f10138
吴成 <1965417564@qq.com>
2023-12-05 14:49:21 +0800

    新增(dto):分页请求数据传输对象

---
+++ dto/AnyRequestPageDto.ts
@@ -0,0 +1,13 @@
+import { IPage } from "interface/IPage";
+import { AnyRequestDto } from "./AnyRequestDto";
+import { AnyPageModel } from "model/AnyPageModel";
+
+/**
+ * # 分页请求数据传输对象
+ */
+export class AnyRequestPageDto<T> extends AnyRequestDto<T> {
+  /**
+   * # 分页信息
+   */
+  page = new AnyPageModel();
+}
